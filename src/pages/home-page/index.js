d32e853015a9a2d1b7eabfc0dd52bb905c99e2e3
iLaD08 <imadeddinelakhdar08@gmail.com>
2021-04-09 19:27:13 +0100

    added loading

--- src/pages/home-page/index.js
+++ src/pages/home-page/index.js
@@ -1,27 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import BeatLoader from "react-spinners/BeatLoader";
 import logo from '../../assests/logo.svg';
 import { HomeLogo, HomeHeader, HomeLink } from './style';
 
-const HomePage = () => (
-    <div>
-        <HomeHeader>
-            <HomeLogo
-                src={logo}
-                alt="logo"
-                title='logo'
-            />
-            <p>
-                A <code>react-boilerplate</code> made by iLaD
-            </p>
-            <HomeLink
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                Learn React
-            </HomeLink>
-        </HomeHeader>
-    </div>
-);
+const HomePage = () => {
+    const [loading, setLoading] = useState(true);
+
+    const override = `
+        display: block;
+        margin: 0 auto;
+        border-color: red;
+    `;
+
+    useEffect(() => {
+        setTimeout(() => setLoading(false), 1000)
+    }, [])
+
+    return (
+        <div >
+            {loading ? (
+                <div className='centred-item'>
+                    <BeatLoader
+                        color='lightblue'
+                        loading={loading}
+                        css={override}
+                        size={20}
+                    />
+                </div>
+            ) : (
+                <HomeHeader>
+                    <HomeLogo
+                        src={logo}
+                        alt="logo"
+                        title='logo'
+                    />
+                    <p>
+                        A <code>react-boilerplate</code> made by iLaD
+                    </p>
+                    <HomeLink
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </HomeLink>
+                </HomeHeader>
+            )
+            }
+        </div>
+    )
+};
 
 export default HomePage;
--- src/pages/other-page/index.js
+++ src/pages/other-page/index.js
@@ -1,14 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import BeatLoader from "react-spinners/BeatLoader";
+import { Button } from '@material-ui/core';
 
-const OtherPage = () => (
-    <div className='centred-item'>
-        <h1>OtherPage</h1>
-        <p>
-            lorem ipsum dolor sit amet consectetur adipisicing elit. hic ea, laboriosam reiciendis sunt perferendis
-            veniam minima autem eius placeat repudiandae molestias quis? quisquam voluptas a blanditiis aliquam libero
-            error sit.
-        </p>
-    </div>
-);
+const OtherPage = () => {
+    const [loading, setLoading] = useState(true);
+
+    const override = `
+        display: block;
+        margin: 0 auto;
+        border-color: red;
+    `;
+
+    useEffect(() => {
+        setTimeout(() => setLoading(false), 1000)
+    }, [])
+
+    return (
+        <div className='centred-item'>
+            {loading ? (
+                <BeatLoader
+                    color='lightblue'
+                    loading={loading}
+                    css={override}
+                    size={20}
+                />
+            ) : (
+                <div className='centred-item'>
+                    <h1>OtherPage</h1>
+                    <p>
+                        lorem ipsum dolor sit amet consectetur adipisicing elit. hic ea, laboriosam reiciendis sunt perferendis
+                        veniam minima autem eius placeat repudiandae molestias quis? quisquam voluptas a blanditiis aliquam libero
+                        error sit.
+                    </p>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        href="/contact"
+                    >
+                        Contact
+            </Button>
+                </div>
+            )
+            }
+        </div>
+    );
+};
 
 export default OtherPage;
