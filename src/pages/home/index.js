16f4f4c8277d795920827a760d90bf70001bc643
iLaD08 <imadeddinelakhdar08@gmail.com>
2021-04-29 21:37:29 +0000

    changed a meme

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -5,7 +5,7 @@ import { HomeDiv, PostsDiv } from "./style";
 
 const Home = () => (
   <HomeDiv>
-    <Topbar />
+    <Topbar/>
     <PostsDiv>
       <Post
         Id='1'
@@ -22,7 +22,7 @@ const Home = () => (
         Date='23/4/2021'
         Author={{
           picture:
-            "https://deadline.com/wp-content/uploads/2020/11/Stephen-Lang-Headshot-Matt-Sayles-e1605093774374.jpg?w=681&h=383&crop=1",
+            "https://i.redd.it/hl18vbd853w61.png",
           username: "Your Daddy",
         }}
         Upload='https://i.redd.it/gno9rwueyhv61.jpg'
