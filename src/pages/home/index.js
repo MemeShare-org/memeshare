f6cf38279f7e6d4acd83b08c92733b93242c0ee3
iLaD08 <imadeddinelakhdar08@gmail.com>
2021-05-02 14:22:34 +0000

    added maping post to home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -6,6 +6,17 @@ import { LoaderDiv, HomeDiv, PostsDiv } from "./style";
 
 const Home = () => {
   const [loading, setLoading] = useState(true);
+  const [posts] = useState([
+    {
+      id: "564675765756765756",
+      date: "2/5/2021",
+      author: {
+        picture: "https://avatars.githubusercontent.com/u/45138843?s=400&u=0a23c06a5fc819e6895feb49d1e3bf6816a2fe29&v=4",
+        username: "iLaD",
+      },
+      upload: "https://i.redd.it/gno9rwueyhv61.jpg",
+    }
+  ]);
 
   useEffect(() => {
     setTimeout(() => setLoading(false), 1000);
@@ -20,18 +31,21 @@ const Home = () => {
       ) : (
         <>
           <Topbar />
-          <PostsDiv>
-            <Post
-              Id='1'
-              Date='23/4/2021'
-              Author={{
-                picture:
-                  "https://avatars.githubusercontent.com/u/45138843?s=400&u=0a23c06a5fc819e6895feb49d1e3bf6816a2fe29&v=4",
-                username: "iLaD",
-              }}
-              Upload='https://i.redd.it/gno9rwueyhv61.jpg'
-            />
-          </PostsDiv>
+          {posts.length ? (
+            <PostsDiv>
+              {posts.map((post, index) => (
+                <Post
+                  key={index}
+                  Id={post.id}
+                  Date={post.date}
+                  Author={post.author}
+                  Upload={post.upload}
+                />
+              ))}
+            </PostsDiv>
+          ) : (
+            <h1>your blabla is empty</h1>
+          )}
         </>
       )}
     </HomeDiv>
