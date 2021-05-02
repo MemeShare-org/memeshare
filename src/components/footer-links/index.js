51647082905754f1e57959e7391ec4e7a125b24a
iLaD08 <imadeddinelakhdar08@gmail.com>
2021-05-02 18:13:21 +0000

    added instagram and twitter link to footer-links

--- src/components/footer-links/index.js
+++ src/components/footer-links/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import YouTubeIcon from "@material-ui/icons/YouTube";
-import InstagramIcon from '@material-ui/icons/Instagram';
+import InstagramIcon from "@material-ui/icons/Instagram";
 import TwitterIcon from "@material-ui/icons/Twitter";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import { LinksDiv } from "./style";
@@ -11,8 +11,12 @@ const FooterLinks = () => (
     <a href='https://github.com/MemeShare-org' target='_blank' rel='noreferrer'>
       <GitHubIcon />
     </a>
-    <InstagramIcon />
-    <TwitterIcon />
+    <a href='https://www.instagram.com/memeshare.01/' target='_blank' rel='noreferrer'>
+      <InstagramIcon />
+    </a>
+    <a href='https://twitter.com/MemeShare4' target='_blank' rel='noreferrer'>
+      <TwitterIcon />
+    </a>
   </LinksDiv>
 );
 
