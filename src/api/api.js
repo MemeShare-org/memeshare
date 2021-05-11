1a6404230d675dd96516482a9d48d1d1a2dbd9ba
iLaD08 <imadeddinelakhdar08@gmail.com>
2021-05-11 17:27:29 +0000

    edited api base url

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: process.env.API_URL || 'http://localhost:3001/',
+  baseURL: process.env.API_URL,
   headers: {
     Authorization: localStorage.getItem('token')
   }
