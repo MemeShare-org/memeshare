2d334f00a957c33b5ae82c6c72241e83f3f1327b
iLaD08 <imadeddinelakhdar08@gmail.com>
2021-06-05 17:18:00 +0100

    edited succes and error classname in sign in and up form

--- src/components/signin-form/style.js
+++ src/components/signin-form/style.js
@@ -1,37 +1,47 @@
 import styled from "styled-components";
 
 export const SigninFormDiv = styled.div`
-	background-color: #151a21;
-	border-radius: 8px;
-	width: 300px;
-	height: auto;
-	padding: 20px;
+  background-color: #151a21;
+  border-radius: 8px;
+  width: 300px;
+  height: auto;
+  padding: 20px;
 
-	a {
-		text-align: left;
-		margin: 10px 0px 10px 15px;
-		color: yellow;
-	}
+  a {
+    text-align: left;
+    margin: 10px 0px 10px 15px;
+    color: yellow;
+  }
 
-	.error {
-		border: 1px solid red;
-	}
+  .error {
+    border: 1px solid red;
+    color: red;
 
-	.succes {
-		border: none;
-	}
+    ::placeholder {
+      color: red;
+    }
+  }
+
+  .succes {
+    border: none;
+    color: white;
+
+    ::placeholder {
+      color: white;
+    }
+  }
 `;
 
 export const SigninMessage = styled.div`
-	text-align: left;
-	margin: 10px 0px 10px 15px;
+  text-align: left;
+  margin: 10px 0px 10px 15px;
 
-	h2 {
-		font-size: 1.875rem;
-		color: gold;
-	}
-	p {
-		font-size: 15px;
-		color: white;
-	}
+  h2 {
+    font-size: 1.875rem;
+    color: gold;
+  }
+  p {
+    font-size: 15px;
+    color: white;
+  }
 `;
