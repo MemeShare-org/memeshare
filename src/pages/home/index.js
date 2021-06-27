fd428a6a19642902ef8699c3368e07120677dc20
iLaD08 <imadeddinelakhdar08@gmail.com>
2021-06-27 15:00:54 +0100

    edited posts in homepage

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -9,7 +9,14 @@ import ProfileCard from "../../components/profile-card/index";
 import ProfileModal from "../../components/profile-modal/index";
 import FriendsList from "../../components/friends-list/index";
 import Post from "../../components/post/index";
-import { LoaderDiv, HomeDiv, PCDiv, PostsDiv } from "./style";
+import {
+  LoaderDiv,
+  HomeDiv,
+  PCDiv,
+  CoolDiv,
+  PostsDiv,
+  EmptyPostsTitle,
+} from "./style";
 
 const Home = () => {
   const { user, setUser } = useContext(UserContext);
@@ -18,36 +25,36 @@ const Home = () => {
   const [loading, setLoading] = useState(true);
   const [friends] = useState([]);
   const [posts] = useState([
-    {
-      id: "564675765756765756",
-      date: "2/5/2021",
-      author: {
-        picture:
-          "https://avatars.githubusercontent.com/u/45138843?s=400&u=0a23c06a5fc819e6895feb49d1e3bf6816a2fe29&v=4",
-        username: "iLaD",
-      },
-      upload: "https://i.redd.it/gno9rwueyhv61.jpg",
-    },
-    {
-      id: "564675765756765756",
-      date: "2/5/2021",
-      author: {
-        picture:
-          "https://b.thumbs.redditmedia.com/Rtu2Mw0L-mx7AeYtHKkbAh75jQbgzy_Hk2UIlLeZFSs.png",
-        username: "RMBG",
-      },
-      upload: "https://i.redd.it/bdnmprwgbow61.jpg",
-    },
-    {
-      id: "564675765756765756",
-      date: "2/5/2021",
-      author: {
-        picture:
-          "https://styles.redditmedia.com/t5_2zmfe/styles/communityIcon_k6po5keenhq61.png?width=256&s=60ef13f941680e157b2166526b7d8458ef36d439",
-        username: "saadmen",
-      },
-      upload: "https://i.redd.it/7jm776qo9kw61.png",
-    },
+    // {
+    //   id: "564675765756765756",
+    //   date: "2/5/2021",
+    //   author: {
+    //     picture:
+    //       "https://avatars.githubusercontent.com/u/45138843?s=400&u=0a23c06a5fc819e6895feb49d1e3bf6816a2fe29&v=4",
+    //     username: "iLaD",
+    //   },
+    //   upload: "https://i.redd.it/gno9rwueyhv61.jpg",
+    // },
+    // {
+    //   id: "564675765756765756",
+    //   date: "2/5/2021",
+    //   author: {
+    //     picture:
+    //       "https://b.thumbs.redditmedia.com/Rtu2Mw0L-mx7AeYtHKkbAh75jQbgzy_Hk2UIlLeZFSs.png",
+    //     username: "RMBG",
+    //   },
+    //   upload: "https://i.redd.it/bdnmprwgbow61.jpg",
+    // },
+    // {
+    //   id: "564675765756765756",
+    //   date: "2/5/2021",
+    //   author: {
+    //     picture:
+    //       "https://styles.redditmedia.com/t5_2zmfe/styles/communityIcon_k6po5keenhq61.png?width=256&s=60ef13f941680e157b2166526b7d8458ef36d439",
+    //     username: "saadmen",
+    //   },
+    //   upload: "https://i.redd.it/7jm776qo9kw61.png",
+    // },
   ]);
 
   const matches = useMediaQuery("(max-width:768px)");
@@ -79,6 +86,12 @@ const Home = () => {
             />
           </PCDiv>
           <FriendsList friends={friends} />
+          <CoolDiv>
+            <div>
+              <h2>Feed</h2>
+              <button>Post</button>
+            </div>
+          </CoolDiv>
           {posts.length ? (
             <PostsDiv>
               {posts.map((post, index) => (
@@ -92,7 +105,7 @@ const Home = () => {
               ))}
             </PostsDiv>
           ) : (
-            <h1>Your feed is empty</h1>
+            <EmptyPostsTitle>Your feed is empty</EmptyPostsTitle>
           )}
         </>
       )}
--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -18,10 +18,39 @@ export const PCDiv = styled.div`
   margin-right: 10px;
 `;
 
+export const CoolDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 19vh;
+
+  div {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 40%;
+  }
+
+  div h2 {
+    color: white;
+  }
+
+  div button {
+    padding: 8px;
+  }
+`;
+
 export const PostsDiv = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  margin-top: 20vh;
+  margin-top: 2.5vh;
+`;
+
+export const EmptyPostsTitle = styled.h1`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 50vh;
+  color: white;
 `;
