f78ff3c4cb18cd636dab50db62bae1658c9cbab7
iLaD08 <imadeddinelakhdar08@gmail.com>
2021-06-30 10:27:00 +0100

    removed youtube icon fromfooter-links

--- src/components/footer-links/index.js
+++ src/components/footer-links/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import YouTubeIcon from "@material-ui/icons/YouTube";
 import InstagramIcon from "@material-ui/icons/Instagram";
 import TwitterIcon from "@material-ui/icons/Twitter";
 import GitHubIcon from "@material-ui/icons/GitHub";
@@ -7,11 +6,13 @@ import { LinksDiv } from "./style";
 
 const FooterLinks = () => (
   <LinksDiv>
-    <YouTubeIcon className='youtube' />
     <a href='https://github.com/MemeShare-org' target='_blank' rel='noreferrer'>
       <GitHubIcon />
     </a>
-    <a href='https://www.instagram.com/memeshare.01/' target='_blank' rel='noreferrer'>
+    <a
+      href='https://www.instagram.com/memeshare.01/'
+      target='_blank'
+      rel='noreferrer'>
       <InstagramIcon />
     </a>
     <a href='https://twitter.com/MemeShare4' target='_blank' rel='noreferrer'>
