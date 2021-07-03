f818fb010df8f140ab691606611282806738cec0
iLaD08 <imadeddinelakhdar08@gmail.com>
2021-07-03 13:42:06 +0100

    created addPost

---
+++ src/actions/posts/addPost.js
@@ -0,0 +1,15 @@
+import API from "../../api/api";
+import { toast } from "react-toastify";
+
+const addPost = ({ post }) => {
+  API.post("/post", post)
+    .then((res) => {
+      toast.success("Posted Successfully!");
+      setTimeout(() => {
+        window.location.href = `/p/${res.data.postId}`;
+      }, 1500);
+    })
+    .catch((err) => console.error(err));
+};
+
+export default addPost;
