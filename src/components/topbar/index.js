5dd8c914347146c1eb07b4b67479b4836d3eb842
iLaD08 <imadeddinelakhdar08@gmail.com>
2021-07-17 13:59:37 +0100

    removed scrolling topbatr

--- src/components/topbar/index.js
+++ src/components/topbar/index.js
@@ -1,39 +1,24 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useHistory } from "react-router-dom";
 import Logo from "../logo/index";
 import Input from "../input";
 import TopbarAccount from "./topbar-account";
-import { TopbarDiv, ScrollTopbar } from "./style";
+import { TopbarDiv } from "./style";
 
 const Topbar = ({ user, setUser }) => {
-  const [isScrolling, SetIsScrolling] = useState(false);
   const history = useHistory();
 
-  useEffect(() => {
-    document.addEventListener("scroll", () => {
-      if (window.scrollY > 100) {
-        SetIsScrolling(true);
-      } else {
-        SetIsScrolling(false);
-      }
-    });
-  });
+
 
   return (
     <div>
-      {isScrolling ? (
-        <ScrollTopbar>
-          <Logo history={history} />
-          <Input Title='Search' Type='search' />
-          <TopbarAccount user={user} setUser={setUser} />
-        </ScrollTopbar>
-      ) : (
+
         <TopbarDiv>
           <Logo history={history} />
           <Input Title='Search' Type='search' />
           <TopbarAccount user={user} setUser={setUser} />
         </TopbarDiv>
-      )}
+
     </div>
   );
 };
