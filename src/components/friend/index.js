6d5d9fb2279e84dc1e3de41fde157155722029dc
iLaD08 <imadeddinelakhdar08@gmail.com>
2021-07-18 18:24:19 +0100

    added link to friend component

--- src/components/friend/index.js
+++ src/components/friend/index.js
@@ -4,7 +4,9 @@ import { FriendDiv } from "./style";
 const Friend = ({ friend }) => (
   <FriendDiv>
     <img src={friend.picture} title={friend.username} alt={friend.username} />
-    <span>{friend.username}</span>
+    <span onClick={() => (window.location.href = `/u/${friend.username}`)}>
+      {friend.username}
+    </span>
   </FriendDiv>
 );
 
