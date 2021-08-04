783f314d30e834e2fba1c8a4e588eed7243417a5
iLaD08 <imadeddinelakhdar08@gmail.com>
2021-08-04 18:14:48 +0100

    formated this file

--- src/actions/auth/signup.js
+++ src/actions/auth/signup.js
@@ -1,16 +1,16 @@
-import API from '../../api/api';
-import { toast } from 'react-toastify';
+import API from "../../api/api";
+import { toast } from "react-toastify";
 
 const signup = ({ values, resetForm }) => {
-    API.post(`/user/signup`, values)
-        .then(res => {
-            resetForm();
-            toast.success('Account created! Go check your email.');
-            setTimeout(() => {
-                window.location.href = `/sign-in`;
-            }, 1500);
-        })
-        .catch(() => toast.error('Account already exist!'))
+  API.post(`/user/signup`, values)
+    .then((res) => {
+      resetForm();
+      toast.success("Account created! Go check your email.");
+      setTimeout(() => {
+        window.location.href = `/sign-in`;
+      }, 1500);
+    })
+    .catch(() => toast.error("Account already exist!"));
 };
 
 export default signup;
